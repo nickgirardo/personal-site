2d3cad524f1f33ea5eca72af09c7c843a2f0201b
Nick Girardo <nickgirardo@gmail.com>
2021-07-12 18:29:06 -0400

    Added page titles with React Helmet
    Page titles are now generated throught the Route title property by React
    Helmet

--- src/components/Route.tsx
+++ src/components/Route.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react';
 
 import { Route as BaseRoute } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 interface Props {
   path: string,
@@ -10,6 +11,9 @@ interface Props {
 
 export const Route = (props: Props): ReactElement => {
   return <BaseRoute path={ props.path }>
+    <Helmet>
+      <title>{ props.title } &mdash; Nick Girardo</title>
+    </Helmet>
     <props.Component />
   </BaseRoute>
 }
