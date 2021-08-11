63b0dde472807480fa1603d9377caccab973dfbd
Nick Girardo <nickgirardo@gmail.com>
2021-08-11 00:55:16 -0400

    Replacing CRA functionality

---
+++ conf/webpack.dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts', '.jsx', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './build'),
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+}
+
