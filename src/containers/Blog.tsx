8f75ca0e2e18c2707d1e8444ded8b1ee0518b202
Nick Girardo <nickgirardo@gmail.com>
2021-08-29 21:32:59 -0400

    Blog: Added proper blog entries

--- src/containers/Blog.tsx
+++ src/containers/Blog.tsx
@@ -1,21 +1,31 @@
 import { ReactElement } from 'react';
 
-import { Link } from 'react-router-dom';
-
 import { Page, BodyType } from '../components/Page';
 import { DefaultItems, NavigationType } from '../components/Navigation';
+import { BlogEntry } from '../components/BlogEntry';
 
+// TODO link rss feed
 export const Blog = ():ReactElement =>
   <Page
     navItems={ DefaultItems }
     navType={ NavigationType.Responsive }
     bodyType={ BodyType.SidebarResponsive }
   >
-    <h2>Blog</h2>
-    <p>
-      <Link to='/blog/sat-solvers-groundwork.html'>Practical SAT Solvers: Groundwork</Link>
-    </p>
-    <p>
-      <Link to='/blog/sat-solvers-sudoku.html'>Practical SAT Solvers: Sudoku</Link>
-    </p>
+    <div>
+      <h2>Blog</h2>
+      <BlogEntry
+        name='Practical SAT Solvers: Sudoku'
+        link='/blog/sat-solvers-sudoku.html'
+        pubDate={ new Date('2021-08-28T04:00:00.000Z') }
+      >
+        A Sudoku solver is constructed as a practical example of solving problems with SAT solvers.
+      </BlogEntry>
+      <BlogEntry
+        name='Practical SAT Solvers: Groundwork'
+        link='/blog/sat-solvers-groundwork.html'
+        pubDate={ new Date('2021-08-28T04:00:00.000Z') }
+      >
+        Exploration of the basics of the Boolean satisfiable problem (SAT) and SAT solvers.
+      </BlogEntry>
+    </div>
   </Page>;
