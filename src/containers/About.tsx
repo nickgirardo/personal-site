73e56f958dceac8938c4ef47321a2833d3daa07d
Nick Girardo <nickgirardo@gmail.com>
2021-08-29 20:41:47 -0400

    About: Added some content

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -3,13 +3,20 @@ import { ReactElement } from 'react';
 import { Page, BodyType } from '../components/Page';
 import { DefaultItems, NavigationType } from '../components/Navigation';
 
+import '../styles/_article.scss';
+
 export const About = ():ReactElement =>
   <Page
     navItems={ DefaultItems }
     navType={ NavigationType.Responsive }
     bodyType={ BodyType.SidebarResponsive }
   >
-    <p>
-      All about me here
-    </p>
+    <article>
+      <p>
+        I am a New York-based software engineer specializing in front-end development and web technologies.  Here is my <a href='/resume.pdf'>resume</a>.
+      </p>
+      <p>
+        I am currently interested in full-time or consulting work (preferrably remote).  If you are interested in working with me, you can contact me at <a href="mailto:nickgirardo@gmail.com">nickgirardo@gmail.com</a>.
+      </p>
+    </article>
   </Page>;
